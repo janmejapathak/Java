b7ae7ab6ca78a0c6715b9c34834a7470fc541050
Janmejay Pathak <janmejaypathak222@gmail.com>
2026-04-05 20:03:37 +0530

    LCM calculation in LCM.java

---
+++ Day3/LCM.java
@@ -0,0 +1,25 @@
+
+// LCM
+
+import java.util.*;
+
+public class sec {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int max = Math.max(a, b);
+        int lcm = 0;
+
+        for(int i = max; i <= a * b; i++){
+            if(i % a == 0 && i % b == 0){
+                lcm = i;
+                break;
+            }
+        }
+
+        System.out.println(lcm);
+    }
+}
