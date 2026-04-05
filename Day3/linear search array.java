2e1a05d5d1da611169f516ca072ec5f9bc6bb78b
Janmejay Pathak <janmejaypathak222@gmail.com>
2026-04-05 20:06:10 +0530

    linear search implementation in Java

---
+++ Day3/linear search array.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+public class sec {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size : ");
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements : ");
+        for(int i = 0; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target : ");
+        int target = sc.nextInt();
+
+        int index = -1;
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] == target){
+                index = i;
+                break;
+            }
+        }
+        if(index != -1){
+            System.out.print("Target found at index : " + index);
+        }else{
+            System.out.print("target not found");
+        }
+       
+    }
+}
