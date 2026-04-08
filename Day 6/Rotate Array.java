fb3f0f3d6092abfbd01eaa01da28d5557e837d54
Janmejay Pathak <janmejaypathak222@gmail.com>
2026-04-08 21:26:59 +0530

    Implement array rotation functionality
    This program takes an array and rotates it by k positions.

---
+++ Day 6/Rotate Array.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class sec {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+
+        rotate(nums, k);
+
+        System.out.println("Rotated array:");
+        for(int num : nums){
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
