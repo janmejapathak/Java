b4f0705e2592202a6e62e3f31321e8c18ad2b690
Janmejay Pathak <janmejaypathak222@gmail.com>
2026-04-09 11:57:42 +0530

    Uncomment code to find minimum in array

---
+++ Day 7/min array .java
@@ -0,0 +1,14 @@
+public class arry{
+    public static void main(String[] args) {
+        int [] arr = {2,45,3,5,6,78};
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]< min) {
+                min = arr[i];
+            }
+            
+        }
+        System.out.println("min = " + min);
+            }
+}
+
