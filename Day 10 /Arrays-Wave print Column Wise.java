702d67fb0570546218a9f27e689900809551b324
Janmejay Pathak <janmejaypathak222@gmail.com>
2026-04-16 09:44:17 +0530

    Add wave print functionality for 2D array
    Implement wave print of a 2D array column-wise based on user input.

---
+++ Day 10 /Arrays-Wave print Column Wise.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] arr = new int[m][n];
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int j = 0; j < n; j++) {
+            if(j % 2 == 0) {
+                for(int i = 0; i < m; i++) {
+                    System.out.print(arr[i][j] + ", ");
+                }
+            } else {
+                for(int i = m - 1; i >= 0; i--) {
+                    System.out.print(arr[i][j] + ", ");
+                }
+            }
+        }
+
+        System.out.print("END");
+    }
+}
