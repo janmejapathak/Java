3c7fbd4b70644a8d0f6541262a1211cc84ae8fe3
Janmejay Pathak <janmejaypathak222@gmail.com>
2026-04-20 18:40:42 +0530

    Add upper triangular matrix printer
    Implemented a program to print the upper triangular matrix of a 2-D array, replacing lower triangle elements with zeros.

---
+++ Day 14 /Print Upper Triangular Matrix.java
@@ -0,0 +1,53 @@
+// You are given a 2-D array and you have to print the elements of upper triangle as it is and zero in the lower triangle.
+
+
+// Input Format
+// First line of the input contains single integers n denoting number of rows and number of columns. Take next n*n elements as input of the matrix.
+
+
+// Constraints
+// 0<n<=100 -10000<=arr[i]<=10000
+
+
+// Output Format
+// The upper half of array with digits and lower as 0.
+
+
+// Sample Input
+// 3
+// 1 2 3
+// 4 5 6
+// 7 8 9
+// Sample Output
+// 1 2 3
+// 0 5 6
+// 0 0 9
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i > j) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
