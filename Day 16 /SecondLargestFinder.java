5b9184becdab03e184b7da92fa9218de843a6855
Janmejay Pathak <janmejaypathak222@gmail.com>
2026-04-22 22:58:23 +0530

    Implement SecondLargestFinder to find second largest number

---
+++ Day 16 /SecondLargestFinder.java
@@ -0,0 +1,20 @@
+public class SecondLargestFinder {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 5, 30, 25};
+
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > first) {
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                second = num;
+            }
+        }
+
+        System.out.println("Second Largest: " + second);
+    }
+}
