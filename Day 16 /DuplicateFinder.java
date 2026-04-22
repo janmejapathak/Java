19a3221e415a9e95e0bd2cdb1a848c203719d415
Janmejay Pathak <janmejaypathak222@gmail.com>
2026-04-22 23:00:20 +0530

    Implement DuplicateFinder to identify duplicates

---
+++ Day 16 /DuplicateFinder.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class DuplicateFinder {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 4, 5, 1, 6};
+
+        Set<Integer> seen = new HashSet<>();
+        Set<Integer> duplicates = new HashSet<>();
+
+        for (int num : arr) {
+            if (!seen.add(num)) {
+                duplicates.add(num);
+            }
+        }
+
+        System.out.println("Duplicate Elements: " + duplicates);
+    }
+}
