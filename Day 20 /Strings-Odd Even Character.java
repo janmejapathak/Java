792e0a1f1c9eefa8cd1f50c7e364411d1a2cfd24
Janmejay Pathak <janmejaypathak222@gmail.com>
2026-04-27 15:54:41 +0530

    Add character replacement based on index parity
    Implement a function to replace characters based on their index parity.

---
+++ Day 20 /Strings-Odd Even Character.java
@@ -0,0 +1,45 @@
+// Take as input S, a string. Write a function that replaces every even character with the character having just higher ASCII code and every odd character with the character having just lower ASCII code. Print the value returned.
+
+
+// Input Format
+// String
+
+
+// Constraints
+// Length of string should be between 1 to 1000.
+
+
+// Output Format
+// String
+
+
+// Sample Input
+// abcg
+// Sample Output
+// badf
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        String ans = "";
+
+        for(int i = 0; i < s.length(); i++){
+
+            char ch = s.charAt(i);
+
+            if(i % 2 == 0){
+                ans = ans + (char)(ch + 1);
+            } else {
+                ans = ans + (char)(ch - 1);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
+
