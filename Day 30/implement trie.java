ec8595c12f14a441d9aec22e36fd697549d4caca
Janmejay Pathak <janmejaypathak222@gmail.com>
2026-07-15 08:45:14 +0530

    Create implement trie.java

---
+++ Day 30/implement trie.java
@@ -0,0 +1,66 @@
+class Trie {
+
+    class TrieNode {
+        TrieNode[] children;
+        boolean isEnd;
+
+        TrieNode() {
+            children = new TrieNode[26];
+            isEnd = false;
+        }
+    }
+
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode current = root;
+
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            if (current.children[index] == null) {
+                current.children[index] = new TrieNode();
+            }
+
+            current = current.children[index];
+        }
+
+        current.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode current = root;
+
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            if (current.children[index] == null) {
+                return false;
+            }
+
+            current = current.children[index];
+        }
+
+        return current.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode current = root;
+
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+
+            if (current.children[index] == null) {
+                return false;
+            }
+
+            current = current.children[index];
+        }
+
+        return true;
+    }
+}
