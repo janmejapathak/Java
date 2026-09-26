31bf1b292682c93786831d8d176118ee9a0fefb1
Janmejay Pathak <janmejaypathak222@gmail.com>
2026-09-26 16:40:10 +0530

    Create Word ladder.java

---
+++ DAY 3Day 35 /Word ladder.java
@@ -0,0 +1,85 @@
+// A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+// Every adjacent pair of words differs by a single letter.
+// Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+// sk == endWord
+// Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+
+ 
+
+// Example 1:
+
+// Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+// Output: 5
+// Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+// Example 2:
+
+// Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+// Output: 0
+// Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+ 
+
+// Constraints:
+
+// 1 <= beginWord.length <= 10
+// endWord.length == beginWord.length
+// 1 <= wordList.length <= 5000
+// wordList[i].length == beginWord.length
+// beginWord, endWord, and wordList[i] consist of lowercase English letters.
+// beginWord != endWord
+// All the words in wordList are unique.
+
+
+
+import java.util.*;
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        
+        Set<String> set = new HashSet<>(wordList);
+        
+        if (!set.contains(endWord)) {
+            return 0;
+        }
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+
+        int level = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                String word = queue.poll();
+                char[] chars = word.toCharArray();
+
+                for (int j = 0; j < chars.length; j++) {
+                    char original = chars[j];
+
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        chars[j] = c;
+
+                        String newWord = new String(chars);
+
+                        if (newWord.equals(endWord)) {
+                            return level + 1;
+                        }
+
+                        if (set.contains(newWord)) {
+                            queue.offer(newWord);
+                            set.remove(newWord);
+                        }
+                    }
+
+                    chars[j] = original;
+                }
+            }
+
+            level++;
+        }
+
+        return 0;
+    }
+}
+
